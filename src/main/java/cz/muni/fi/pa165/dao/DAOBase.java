9baea0e19bb40393f777b3c7956ea4e95654ffcd
hradecek <ivohradek@gmail.com>
2015-10-27 22:04:40 +0100

    Use spring @Transactional

--- src/main/java/cz/muni/fi/pa165/dao/DAOBase.java
+++ src/main/java/cz/muni/fi/pa165/dao/DAOBase.java
@@ -1,25 +1,26 @@
 package cz.muni.fi.pa165.dao;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 /**
  * @author Jana Cechackova
  */
 
 public class DAOBase {
-    
+
     private EntityManager entityManager;
-    
+
     @Transactional
     public EntityManager getEntityManager() {
         return entityManager;
-    } 
-     
+    }
+
     @PersistenceContext
     public void setEntityManager(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
-    
+
 }
