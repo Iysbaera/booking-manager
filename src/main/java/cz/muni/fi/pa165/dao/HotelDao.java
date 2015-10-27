c91ac6fbcec5bde41abcd0b272ea5bf922103eec
hradecek <ivohradek@gmail.com>
2015-10-27 22:04:55 +0100

    Add required methods.

--- src/main/java/cz/muni/fi/pa165/dao/HotelDao.java
+++ src/main/java/cz/muni/fi/pa165/dao/HotelDao.java
@@ -1,18 +1,24 @@
 package cz.muni.fi.pa165.dao;
 
 import cz.muni.fi.pa165.entity.Hotel;
+import cz.muni.fi.pa165.entity.Room;
+
+import java.util.Collection;
 
 /**
  * @author Jana Cechackova
  */
 public interface HotelDao {
-    
+
     public void addHotel(Hotel hotel);
-    
+
     public void updateHotel(Hotel hotel);
-    
+
     public void deleteHotel(Hotel hotel);
-    
+
     public Hotel getHotelById(Long Id);
-    
+
+    public Collection<Hotel> findAllHotels();
+
+    public Collection<Room> findFreeRooms();
 }
