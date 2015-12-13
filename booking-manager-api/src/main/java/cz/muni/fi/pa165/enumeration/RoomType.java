9292d18cefbb19cec33b2d3f2f430bbee22c0cbf
hradecek <ivohradek@gmail.com>
2015-12-13 14:51:26 +0100

    Moved enumeration of Room Type.

---
+++ booking-manager-api/src/main/java/cz/muni/fi/pa165/enumeration/RoomType.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pa165.enumeration;
+
+/**
+ * Enumeration representing type of the room.
+ *
+ * @author Ivo Hradek
+ */
+public enum RoomType {
+    SingleRoom, DoubleRoom, DeluxeRoom
+}
